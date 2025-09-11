5ba1676ab995d4ceb7ecad704bd3026fb7824519
pouria drd <pouriadrd@gmail.com>
2025-09-11 19:48:12 +0330

    added refresh api view

--- authentication/api/urls.py
+++ authentication/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import LoginAPIView, RegisterView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
 ]
