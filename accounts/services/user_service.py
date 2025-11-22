c45f8fa94bada073ad74a6fd3d6b84e1aa382199
pouria drd <pouriadrd@gmail.com>
2025-11-22 13:42:01 +0330

    created register user in user service

--- accounts/services/user_service.py
+++ accounts/services/user_service.py
@@ -1,4 +1,36 @@
+from django.db import transaction
+from rest_framework.exceptions import ValidationError
+
+
+from core.constants import UserRole
+from accounts.models import UserModel
+from accounts.repositories import UserRepository
+
+
 class UserService:
     """Service layer for user-related business logic."""
 
-    pass
+    @staticmethod
+    @transaction.atomic
+    def register_user(
+        email: str, role: UserRole = UserRole.USER, **extra_fields
+    ) -> UserModel:
+        """
+        Register a new user with profile and settings in a single transaction.
+
+        Raises:
+            ValidationError: If a user with the same email exists.
+
+        Returns:
+            UserModel: Newly created user instance.
+        """
+        # Check if user with email already exists
+        if UserRepository.get_user_by_email(email):
+            raise ValidationError(
+                f"A user with email '{email}' already exists.", code="user_exists"
+            )
+        # Create user, profile, and settings
+        user = UserRepository.create_user(email=email, role=role, **extra_fields)
+        UserRepository.create_profile(user)
+        UserRepository.create_settings(user)
+        return user
